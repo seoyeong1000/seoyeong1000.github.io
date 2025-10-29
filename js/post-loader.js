ff49bd8da26a17ee058366af8fec6f8693b623d4
seoyeong1000 <seoyeong0035@gmail.com>
2025-10-29 11:14:39 +0900

    feat: Giscus ?볤? ?쒖뒪???ㅼ젙 ?꾨즺

--- js/post-loader.js
+++ js/post-loader.js
@@ -149,14 +149,14 @@
     const script = document.createElement("script");
     script.src = "https://giscus.app/client.js";
     script.setAttribute("data-repo", "seoyeong1000/seoyeong1000.github.io");
-    script.setAttribute("data-repo-id", "YOUR_REPO_ID"); // Giscus 설정에서 가져온 값으로 변경 필요
+    script.setAttribute("data-repo-id", "R_kgDOQKfwqw");
     script.setAttribute("data-category", "General");
-    script.setAttribute("data-category-id", "YOUR_CATEGORY_ID"); // Giscus 설정에서 가져온 값으로 변경 필요
+    script.setAttribute("data-category-id", "DIC_kwDOQKfwq84CxMbe");
     script.setAttribute("data-mapping", "pathname");
     script.setAttribute("data-strict", "0");
     script.setAttribute("data-reactions-enabled", "1");
-    script.setAttribute("data-emit-metadata", "1");
-    script.setAttribute("data-input-position", "top");
+    script.setAttribute("data-emit-metadata", "0");
+    script.setAttribute("data-input-position", "bottom");
     script.setAttribute("data-theme", "preferred_color_scheme");
     script.setAttribute("data-lang", "ko");
     script.setAttribute("data-loading", "lazy");
